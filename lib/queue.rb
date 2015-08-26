8c2beb1d854da6e6638a743e842238f86b0c7ee2
Lia Skalkos <hello@liaskalkos.net>
2015-08-26 11:26:20 -0400

    Specs passing

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,25 @@
 require_relative 'stack'
+require 'pry'
 
 class Queue
 
+  def initialize
+    @stack1 = Stack.new
+    @stack2 = Stack.new
+  end
+
+  def enqueue(data)
+    @stack1.push(data)
+  end
+
+  def dequeue
+    if @stack2.size == 0
+      while @stack1.size > 0
+        @stack2.push(@stack1.pop)
+      end
+    end
+
+    @stack2.pop
+
+  end
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,17 @@
 class Stack
+  def initialize
+    @elements = []
+  end
 
+  def push(data)
+    @elements << data
+  end
+
+  def pop
+    @elements.pop
+  end
+
+  def size
+    @elements.size
+  end
 end
